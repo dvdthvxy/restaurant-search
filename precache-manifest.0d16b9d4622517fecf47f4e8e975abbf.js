6b4396a6c93c88606972053650498ccdeffb3651
dvdthvxy <david.thavixay@gmail.com>
2020-04-29 23:05:41 -0400

    Updates

---
+++ precache-manifest.0d16b9d4622517fecf47f4e8e975abbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b9854f1d9296a9f98040687786314a",
+    "url": "/restaurant-search/index.html"
+  },
+  {
+    "revision": "b8ce2223d61656aa9d5e",
+    "url": "/restaurant-search/static/css/main.9bca530d.chunk.css"
+  },
+  {
+    "revision": "dcdf87217aaa670035d8",
+    "url": "/restaurant-search/static/js/2.20de66df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/restaurant-search/static/js/2.20de66df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8ce2223d61656aa9d5e",
+    "url": "/restaurant-search/static/js/main.0bc79f96.chunk.js"
+  },
+  {
+    "revision": "41e72d4915b871ce7cc1",
+    "url": "/restaurant-search/static/js/runtime-main.71c40e1e.js"
+  }
+]);
